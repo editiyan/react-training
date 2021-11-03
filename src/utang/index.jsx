83c8bff07fc199d8434ef0450cd98a82ccd78c19
editiyan <aloysius.editiyan@gmail.com>
2021-11-03 16:33:39 +0700

    gh pages

--- src/utang/index.jsx
+++ src/utang/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import TitleComponent from '../day2-practice/inline/TitleComponent'
 import ButtonKlik from '../day2-practice/components/Button'
@@ -6,6 +6,11 @@ import ButtonKlik from '../day2-practice/components/Button'
 export default function Utang() {
     const utang = useSelector(state => state.utang);
     const dispatch = useDispatch()
+    const [jumlah, setjumlah] = useState(0)
+
+    const updateJumlah = () => {
+        setjumlah(document.getElementById("jumlah").value)
+    }
 
     return (
         <div className="mt-4">
@@ -13,13 +18,14 @@ export default function Utang() {
             <br />
             <hr />
             <div className="mt-4">Apakah mau ngutang?</div>
+            {/* <input type="text" id="jumlah" onChange={updateJumlah()} value={0}> 0</input> */}
             <br />
-            <button className="rounded-lg bg-green-500 border-green-500 mx-4 my-2 mb-4 py-2 px-2 text-white font-bold" onClick={() => dispatch({type: "TAMBAH"})}>Tambah Utang Rp. 1000</button>
-            <button className="rounded-lg bg-yellow-500 border-yellow-500 mx-4 my-2 mb-4 py-2 px-2 text-white font-bold" onClick={() => dispatch({type: "BAYAR"})}>Bayar Utang Rp. 1000</button>
+            <button className="rounded-lg bg-green-500 border-green-500 mx-4 my-2 mb-4 py-2 px-2 text-white font-bold" onClick={() => dispatch({type: "TAMBAH", payload: 1000})}>Tambah Utang Rp. 1000</button>
+            <button className="rounded-lg bg-yellow-500 border-yellow-500 mx-4 my-2 mb-4 py-2 px-2 text-white font-bold" onClick={() => dispatch({type: "BAYAR", payload: 1000})}>Bayar Utang Rp. 1000</button>
             <hr />
             <div className="mt-4">Apakah Anda mau ngelunasin?</div>
             <br/>
-            <ButtonKlik klik={() => dispatch({type: "LUNASI"})} text={"LUNASI DONG"}/>
+            <ButtonKlik klik={() => dispatch({type: "LUNASI"})} text={"Lunasi Dong"}/>
 
             {/* <ul class="list-inside bg-rose-200 ...">
                 {users.map(user => (
--- src/utang/utangReducer.js
+++ src/utang/utangReducer.js
@@ -1,13 +1,13 @@
 const initialState = {
-    utang: 100000
+    utang: 0
 }
 
 const utangReducer = (state=initialState, action) => {
     switch (action.type) {
         case "TAMBAH":
-            return { utang: state.utang + 1000 }
+            return { utang: state.utang + action.payload }
         case "BAYAR":
-            return { utang: state.utang - 1000 }
+            return { utang: state.utang - action.payload }
         case "LUNASI":
             return { utang: 0 }
         default:
